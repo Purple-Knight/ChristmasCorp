02451d000c561d3bb10eec9ff5767554000c16ed
EstebanNunez <esteban78570@gmail.com>
2023-10-31 11:38:42 +0100

    Init cpp
    - Add DayDataAsset

---
+++ Source/EDT/DayDataAsset.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "DayDataAsset.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class EDT_API UDayDataAsset : public UPrimaryDataAsset
+{
+	GENERATED_BODY()
+	
+};
