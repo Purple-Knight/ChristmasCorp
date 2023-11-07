bcd30485ab8a63c4d278e58f811c5ee6f33b4d3b
EstebanNunez <esteban78570@gmail.com>
2023-11-07 10:46:06 +0100

    [Add] Game Settings

---
+++ Source/EDT/GameSettings.h
@@ -0,0 +1,23 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DeveloperSettings.h"
+#include "GameSettings.generated.h"
+
+/**
+ * 
+ */
+UCLASS(Config = Game, defaultconfig, meta = (ClassName = "Game Settings"))
+class EDT_API UGameSettings : public UDeveloperSettings
+{
+	GENERATED_BODY()
+	
+public:
+	UPROPERTY(Config, EditAnywhere, BlueprintReadOnly, Category = "Schedule")
+	TSoftObjectPtr<UDataTable> BlockList = nullptr;
+
+	UPROPERTY(Config, EditAnywhere, BlueprintReadOnly, Category = "Schedule")
+	TSoftObjectPtr<UDataTable> DayList = nullptr;
+};
